0e16da767f6d2ec321a030a77dc4068e0692e973
Itachi1998 <103276338+Itachi1998@users.noreply.github.com>
2025-06-11 09:32:12 -0400

    Update PROJECTNAME.h

--- templates/default.c/PROJECTNAME.h
+++ templates/default.c/PROJECTNAME.h
@@ -1,1 +1,11 @@
+/* PROJECTNAME.h */
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <errno.h>
+#include <birchutils.h>
 
