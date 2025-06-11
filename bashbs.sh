dcaf938a1f43575afd712518e014871774a1166e
Itachi1998 <103276338+Itachi1998@users.noreply.github.com>
2025-06-11 12:04:18 -0400

    Update bashbs.sh

--- bashbs.sh
+++ bashbs.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+
 pname= $1
+pdir = "./${pname}"
 tdir= /templates
 pdir= 
 
 if [ -z "$pname" ]; then
    usage
    exit 1
-elif 
+elif ! [ -d "$tdir" ]; then
+   >&2 echo "Unable to find template for: $tdir"
+   exit 2
+elif [ -d "$pdir" ]; then
+   >&2 echo "Project dir already exists: $pdir"
+   exit 3
+fi
+
+cur="$PWD"
+cd $dir
+echo "Select a template"
+select x in *; do
+   template="$x"
+   break
+done
+
